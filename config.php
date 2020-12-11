147931cdeb83f070cac7b7bbb33655c226fbffbb
Leda909 <ko9zad@gmail.com>
2020-12-11 00:54:03 +0000

    push up files

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$conn = mysqli_connect("localhost","root","","silk_shop");
+
+// Check connection
+if (mysqli_connect_errno())
+   {
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+
+# For default timezone setup
+date_default_timezone_set('UTC');
+
+session_start();
+
+?>
