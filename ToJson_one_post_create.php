62e7da99089077c7c184957dcc82be043aacdd3b
Leda909 <ko9zad@gmail.com>
2020-12-14 18:31:07 +0000

    add data is not empty + error message functions

--- ToJson_one_post_create.php
+++ ToJson_one_post_create.php
@@ -9,27 +9,42 @@ header("Access-Control-Allow-Methods: POST");
 
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
-    $myObj=new Gallery();
+    // make sure data is not empty
+    if(
+    !empty($data->product_category) &&
+    !empty($data->product_type) &&
+    !empty($data->product_image) &&
+    !empty($data->product_title)
+    ){
 
-    // for setting items property values -->    $product->name = $data->name; 
-    $myObj->set_product_category($data->product_category);
-    $myObj->set_product_type($data->product_type);
-    $myObj->set_product_image($data->product_image);
-    $myObj->set_product_title($data->product_title);
+        $myObj=new Gallery();
 
-$create = create_gallery($myObj);
-// echo json_encode($myObj);
+        // for setting items property values -->    $product->name = $data->name; 
+        $myObj->set_product_category($data->product_category);
+        $myObj->set_product_type($data->product_type);
+        $myObj->set_product_image($data->product_image);
+        $myObj->set_product_title($data->product_title);
 
-if($myObj){
-    $myJSON_Created = json_encode($myObj);
-    echo json_encode(array("message" => "Product has been successfully inserted."));
-    echo $myJSON_Created;
+        $create = create_gallery($myObj);
+        // echo json_encode($myObj);
+        
+        if($myObj){
+            $myJSON_Created = json_encode($myObj);
+            echo json_encode(array("message" => "Product has been successfully inserted."));
+            echo $myJSON_Created;
+            } 
+            // if unable to create the product, tell the user
+            else {
+                http_response_code(503);
+                // tell the user product does not created
+                echo json_encode(array("message" => "Product couldn't be created."));
+            }
     } 
-    // if unable to create the product, tell the user
-    else {
-        http_response_code(503);
-        // tell the user product does not created
-        echo json_encode(array("message" => "Product couldn't be created."));
-    }
+    // tell the user data is incomplete
+    else { 
+        http_response_code(400);
+ 
+        echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
 
+    }
 ?>
