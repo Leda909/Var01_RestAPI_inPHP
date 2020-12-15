fdb7fd46f846a86f0f68acbf25b07d9632b0cacc
Leda909 <ko9zad@gmail.com>
2020-12-15 08:34:13 +0000

    $sql

--- data_model_gallery_delete.php
+++ data_model_gallery_delete.php
@@ -7,9 +7,9 @@ function delete_item_from_gallery($delete_item){
     
     // test how able to reach get(s) on the Gallery class
     // $temp=$delete_item->get_product_id();
-
-	$sql = "DELETE FROM gallery WHERE product_id = '.$delete_item->product_id().'";
-			
+	
+	$sql = "DELETE FROM gallery WHERE product_id='".$delete_item->product_id()."'";
+	
 	mysqlI_query($conn, $sql);
 
 	if(mysqli_affected_rows($conn)>0){
